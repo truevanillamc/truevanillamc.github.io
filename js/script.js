d02c861db4311ff71f64a62f24531770ff2fce54
truevanillamc <realvanillamc@gmail.com>
2025-05-14 18:26:11 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,20 +4,8 @@ document.addEventListener('DOMContentLoaded', () => {
   let currentImage = 1;
 
   setInterval(() => {
-    // Fade out
-    image.classList.add('fade-out');
-    image.classList.remove('fade-in');
-
-    // After fade-out, change image and fade back in
-    setTimeout(() => {
-      currentImage = (currentImage % imageCount) + 1;
-      image.src = `img/img-${currentImage}.png?v=3`;
-
-      // Force reflow to restart animation
-      void image.offsetWidth;
-
-      image.classList.remove('fade-out');
-      image.classList.add('fade-in');
-    }, 1000); // match CSS transition time
-  }, 3000);
+    currentImage = (currentImage % imageCount) + 1;
+    image.src = `img/img-${currentImage}.png?v=3`;
+    void image.offsetWidth;
+  }, 4000);
 });
