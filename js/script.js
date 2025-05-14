02726a79ce282b8a30c369622f4ed22d05151458
truevanillamc <realvanillamc@gmail.com>
2025-05-14 18:00:29 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
-const image = document.getElementById('slideshow');
-const imageCount = 4; // how many img
-let currentImage = 1;
-setInterval(() => {
-  image.classList.add('fade-out');
-  setTimeout(() => {
-    currentImage = (currentImage % imageCount) + 1;
-    image.src = `img/img-${currentImage}.png?v=2`;
-    image.classList.remove('fade-out');
-  }, 1000);
-}, 8000); // 15 sec
+document.addEventListener('DOMContentLoaded', () => {
+  const image = document.getElementById('slideshow');
+  const imageCount = 4;
+  let currentImage = 1;
+
+  setInterval(() => {
+    image.classList.add('fade-out');
+    setTimeout(() => {
+      currentImage = (currentImage % imageCount) + 1;
+      image.src = `img/img-${currentImage}.png?v=2`;
+      image.classList.remove('fade-out');
+    }, 1000);
+  }, 8000);
+});
