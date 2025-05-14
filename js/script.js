e97190c9bebf4c5025c14defcfde7114e3d71488
truevanillamc <realvanillamc@gmail.com>
2025-05-14 18:31:15 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,11 +4,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
   let currentImage = Math.floor(Math.random() * imageCount) + 1;
 
-  image.src = `img/img-${currentImage}.png?v=3`;
+  image.src = `img/img-${currentImage}.png?v=5`;
 
   setInterval(() => {
     currentImage = (currentImage % imageCount) + 1;
-    image.src = `img/img-${currentImage}.png?v=3`;
+    image.src = `img/img-${currentImage}.png?v=5`;
     void image.offsetWidth; // not sure what this does, google reccommended it ¯\_(ツ)_/¯
   }, 4000);
 });
