6c6ca430d586cde6561ab0e6c480017d89333d21
truevanillamc <realvanillamc@gmail.com>
2025-05-14 18:30:45 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
   const image = document.getElementById('slideshow');
   const imageCount = 8;
-  let currentImage = 1;
+
+  let currentImage = Math.floor(Math.random() * imageCount) + 1;
+
+  image.src = `img/img-${currentImage}.png?v=3`;
 
   setInterval(() => {
     currentImage = (currentImage % imageCount) + 1;
     image.src = `img/img-${currentImage}.png?v=3`;
-    void image.offsetWidth;
+    void image.offsetWidth; // not sure what this does, google reccommended it ¯\_(ツ)_/¯
   }, 4000);
 });
